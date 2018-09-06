c2681243a0da26872d3f8754b65ec8ff9460d35d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-09-06 03:53:13 -0400

    project initially complete

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function showAssignment2() {
+  document.getElementById('assignment_2').style.display='block';
+  document.getElementById('assignment_3').style.display='none';
+}
+
+function showAssignment3() {
+  document.getElementById('assignment_2').style.display='none';
+  document.getElementById('assignment_3').style.display='block';
+}
